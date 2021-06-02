d00e26ad33f1286241a2ab8a127e43258b6e0264
Michael Kret <dirtysculptor@gmail.com>
2021-06-02 08:18:57 +0300

    need to write editNote functionality

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 import { data } from "./data/data.js";
-import { renderNotes, saveNote, createNote } from "./js/app.js";
+import {
+  renderNotes,
+  saveNote,
+  createNote,
+  displaySummary,
+  deleteNote,
+  archiveNote,
+  editNote,
+} from "./js/app.js";
 
-renderNotes(data.notesList, ".content");
+function reRender(archive = false) {
+  const notes = archive ? data.notesListArchive : data.notesList;
+  renderNotes(notes, ".content", archive);
+  displaySummary(data.category, notes, ".summary");
+  document.querySelectorAll(".note__delete").forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      deleteNote(e, data);
+    })
+  );
+  document.querySelectorAll(".note__archive").forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      archiveNote(e, data, archive);
+    })
+  );
+  document.querySelectorAll(".note__edit").forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      editNote(e);
+    })
+  );
+}
+
+reRender();
 
 document
   .querySelector(".header__create-btn")
@@ -10,5 +39,13 @@ document
 document.querySelector(".create-note").addEventListener("submit", (e) => {
   e.preventDefault();
   saveNote(e, data);
-  renderNotes(data.notesList, ".content");
+  reRender();
+});
+
+document.querySelector(".nav__all").addEventListener("click", () => {
+  reRender();
+});
+
+document.querySelector(".nav__archive").addEventListener("click", () => {
+  reRender(true);
 });
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-export const renderNotes = (notes, htmlSelector) => {
+export const renderNotes = (notes, htmlSelector, archive = false) => {
   const mountPoint = document.querySelector(htmlSelector);
   mountPoint.innerHTML = "";
   notes.forEach((note) => {
@@ -13,7 +13,7 @@ export const renderNotes = (notes, htmlSelector) => {
     </div>
     <div class="note__settings">
       <button class="note__edit">Edit</button>
-      <button class="note__archive">Archive</button>
+      <button class="note__archive">${archive ? "Una" : "A"}rchive</button>
       <button class="note__delete">Delete</button>
     </div>
     `;
@@ -32,8 +32,6 @@ export const saveNote = (form, data) => {
   const dd = creationDate.getDate();
   const mm = creationDate.getMonth();
   const yyyy = creationDate.getFullYear();
-  console.log(form.target["category"].value);
-  console.log(dd + "/" + mm + "/" + yyyy);
   const newNote = {
     id: 2,
     created: dd + "/" + mm + "/" + yyyy,
@@ -44,23 +42,58 @@ export const saveNote = (form, data) => {
   document.querySelector(".header__create-btn").style.visibility = "visible";
   document.querySelector(".create-note").style.display = "none";
   document.querySelector(".content").style.display = "block";
+  form.target["text"].value = "";
+  form.target["category"].value = "Task";
 };
 
-export const displaySummary = (category, notes) => {
+export const displaySummary = (category, notes, htmlSelector) => {
   const mountPoint = document.querySelector(htmlSelector);
   mountPoint.innerHTML = "";
   const summaryDiv = document.createElement("div");
   category.forEach((entry) => {
     const notesByCategory = notes.filter(
-      (note) => note.category === category
+      (note) => note.category === entry
     ).length;
+    summaryDiv.insertAdjacentHTML(
+      "beforeend",
+      `<p>${entry}: <span>${notesByCategory}</span></p>`
+    );
+  });
+  mountPoint.appendChild(summaryDiv);
+};
+
+export const editNote = (e) => {
+  console.log(e.target.parentNode.parentNode);
+};
+
+export const deleteNote = (e, data) => {
+  const noteDiv = e.target.parentNode.parentNode;
+  noteDiv.style.display = "none";
+  const noteId = parseInt(noteDiv.children[0].id);
+  data.notesList = data.notesList.filter((note) => {
+    return note.id !== noteId;
   });
 };
 
-export const editNote = () => {};
-
-export const deleteNote = () => {};
-
-export const archiveNote = () => {};
-
-export const unarchiveNote = () => {};
+export const archiveNote = (e, data, unarchive = false) => {
+  const noteDiv = e.target.parentNode.parentNode;
+  noteDiv.style.display = "none";
+  const noteId = parseInt(noteDiv.children[0].id);
+  if (!unarchive) {
+    const archiveIndex = data.notesList.findIndex(
+      (entry) => entry.id === noteId
+    );
+    data.notesListArchive.push(data.notesList[archiveIndex]);
+    data.notesList = data.notesList.filter((note) => {
+      return note.id !== noteId;
+    });
+  } else {
+    const archiveIndex = data.notesListArchive.findIndex(
+      (entry) => entry.id === noteId
+    );
+    data.notesList.push(data.notesListArchive[archiveIndex]);
+    data.notesListArchive = data.notesListArchive.filter((note) => {
+      return note.id !== noteId;
+    });
+  }
+};
